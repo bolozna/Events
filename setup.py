3724ad21456531c3a614ec7866b45a557098fb0a
Mikko Kivelä <bolozna@gmail.com>
2016-07-26 18:01:12 +0300

    Fixed a bug where setup.py didn't copy the extension module code for some platforms.

--- setup.py
+++ setup.py
@@ -6,12 +6,25 @@ setup.py file for Python wrapping of a C++ network data structure
 
 from distutils.core import setup, Extension
 
+#The copying comes before building (not for all systems/versions). We use code from:
+#http://stackoverflow.com/questions/12491328/python-distutils-not-include-the-swig-generated-module
+from distutils.command.build import build
+
+class CustomBuild(build):
+    sub_commands = [
+        ('build_ext', build.has_ext_modules), 
+        ('build_py', build.has_pure_modules),
+        ('build_clib', build.has_c_libraries), 
+        ('build_scripts', build.has_scripts),
+    ]
+
 
 eventpy_module = Extension('eventpy._eventpy_module',['eventpy/eventpy_module.i'],
                            swig_opts=["-c++"]
                            )
 
 setup (name = 'eventpy',
+       cmdclass={'build': CustomBuild},
        version = '0.1',
        author      = "Mikko Kivela",
        description = """Python wrapping of a C++ event list data structure.""",
